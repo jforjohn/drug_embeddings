28f7a7dce753eae90861972db779df92fa23cd92
Nicolás Pascual González <nicolaspascualgonzalez@gmail.com>
2019-03-06 17:52:10 +0100

    Changed module structure :tada:

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,7 @@
 from input_output.parser import Parser
 from input_output.writer import Writer
-from tokenizer import tokenize
-from pipeline.rules.token_classifier import classify_token, classify_tokens
+from preprocessing.tokenizer import tokenize
+from models.rules import classify_token, classify_tokens
 
 base_folder = '../resources/Test-DDI/DrugBank'
 
