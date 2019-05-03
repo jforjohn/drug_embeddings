767fbe58f2ccfabd36a397db6789d7522f1e5b42
jforjohn <giannisotiro@gmail.com>
2019-05-03 20:06:27 +0100

    PARSER: add all offsets

--- src/input_output/parser/parser.py
+++ src/input_output/parser/parser.py
@@ -27,7 +27,7 @@ class Parser(object):
                     offsets = [
                         [int(index) for index in offset.split('-')]
                         for offset in e.attributes['charOffset'].value.split(';')
-                    ][0]
+                    ]
                     entities.append(DrugEntity(
                         de_id=e.attributes['id'].value,
                         offsets=offsets,
