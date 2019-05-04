2c0752cbe323dd9eefbd78435838bab10e600612
jforjohn <giannisotiro@gmail.com>
2019-05-04 12:16:04 +0100

    DL: add deep learning models

---
+++ src/models/dl/myModel.py
@@ -0,0 +1,33 @@
+from keras.layers import Dense
+from keras.layers import LSTM, GRU
+from keras.regularizers import L1L2
+from keras.models import Model, Input
+from keras.layers import LSTM, Embedding, Dense, TimeDistributed, Dropout, Bidirectional
+from keras_contrib.layers import CRF
+
+
+def architecture(config, n_words, n_tags, max_len, emb_dim):
+  arch_type = config['arch_type']
+  neurons_rnn = config['neurons_rnn']
+  neurons_dense = config['neurons_dense']
+  rec_drop = config['rec_drop']
+  impl = config['impl']
+  if arch_type == 'BLSTM':
+    input_model = Input(shape=(max_len,))
+    model = Embedding(input_dim=n_words + 1,
+                      output_dim=emb_dim,
+                      input_length=max_len,
+                      mask_zero=True)(input_model)  # 20-dim embedding
+    model = Bidirectional(LSTM(units=neurons_rnn,
+                          return_sequences=True,
+                          recurrent_dropout=rec_drop,
+                          implementation=impl))(model)  # variational biLSTM
+    model = TimeDistributed(Dense(neurons_dense, activation="relu"))(model)  # a dense layer as suggested by neuralNer
+    crf = CRF(n_tags)  # CRF layer
+    out = crf(model)  # output
+
+    model = Model(input, out)
+
+  print(model.summary())
+
+  return crf, model
